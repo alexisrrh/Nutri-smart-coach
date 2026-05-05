38604947b46ff7d4d25488384f2af9ef73c8ef60
Alexis <alexis@gmail.com>
2026-05-05 21:30:38 +0200

    preparando backend para produccion

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
+      "https://nutri-coach-ia.vercel.app"
     ],
     credentials: true,
   })
