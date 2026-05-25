da2f8f2287a471f2823733fe2eb90f149b02e267
alexisrrh <alexisrrh123@gmail.com>
2026-05-25 09:32:35 +0200

    separar backend pp desde server

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,6 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import { corsOptions } from "./config/cors.js";
-import checkinsRoutes from "./routes/checkins.routes.js";
-import dietsRoutes from "./routes/diets.routes.js";
-import mealsRoutes from "./routes/meals.routes.js";
+import app from "./app.js";
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(cors(corsOptions));
-
-app.use(express.json({ limit: "10mb" }));
-app.use("/", checkinsRoutes);
-app.use("/", dietsRoutes);
-app.use("/", mealsRoutes);
-
-app.get("/", (req, res) => {
-  res.json({
-    ok: true,
-    message: "NutriSmartCoach backend activo",
-  });
-});
-
-app.get("/health", (req, res) => {
-  res.json({
-    ok: true,
-    service: "nutrismartcoach-api",
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`Servidor activo en puerto ${PORT}`);
